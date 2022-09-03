73f78b7731ccbc8476570742b0ecc20296660784
Jaden Lund <lundjaden@gmail.com>
2022-09-03 14:35:19 -0400

    commit tree 4e1b0fb793efdb45f5bae63313595405bd42eea1

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,19 @@
 class Triangle
   # write code here
+  attr_accessor :side1, :side2, :side3
+  attr_reader :equilateral, :isosceles, :scalene
+
+def initialize side1, side2, side3
+@side1 = side1
+@side2 = side2
+@side3 = side3
+end
+  # if side1 == side2 = side3
+  #   equilateral
+  # end
+
+
+
+  class TriangleError < StandardError
+  end
 end
